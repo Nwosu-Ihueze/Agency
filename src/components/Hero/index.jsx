16f2f1346009b2af19ec584df5a19d0055fb01ee
adaihueze <darkmage@adaihuezes-MacBook-Pro.local>
2024-04-04 21:42:48 -0400

    updated design

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -27,22 +27,22 @@ const HeroSection = () => {
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
             >
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                🚀 <span className="text-[#FA7436]">Launch </span>Your Idea with Us! 🚀
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight font-volkhov">
+                🚀 <span className="text-[#FA7436] font-volkhov">Launch </span>Your Idea with Us! 🚀
                 </h1>
-                <p className="mb-12 text-base !leading-relaxed text-[#595959] sm:text-lg md:text-xl">
+                <p className="mb-12 text-base !leading-relaxed text-[#595959] sm:text-lg md:text-xl font-poppins">
                 At 7Decagon, we&apos;re here to make your vision a reality. Whether you&apos;re a startup or an established company, we specialize in crafting your Minimum Viable Product (MVP) and bringing your ideas to life.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                 <div
                     // onClick={handleOpenEmail}
-                    className="rounded-md bg-[transparent] border border-xl border-[#FA7436] px-8 py-4 text-base font-semibold text-[#FA7436] duration-300 ease-in-out hover:bg-[#FA7436] hover:text-white" onClick={handleClick}
+                    className="rounded-md bg-[transparent] font-poppins border border-xl border-[#FA7436] px-8 py-4 text-base font-semibold text-[#FA7436] duration-300 ease-in-out hover:bg-[#FA7436] hover:text-white" onClick={handleClick}
                 >
                     Let&apos;s Talk
                 </div>
                 <Link
                     href="#pricing"
-                    className="inline-block rounded-md bg-[#FA7436] px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-opacity-90"
+                    className="inline-block rounded-md bg-[#FA7436] font-poppins px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-opacity-90"
                 >
                     See Pricing
                 </Link>
--- src/components/Pricing/index.jsx
+++ src/components/Pricing/index.jsx
@@ -1,71 +1,71 @@
 // components/PricingSection.jsx
 
 const Pricing = () => {
-    return (
-      <section id="pricing" className="bg-[#F7F8FC] py-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-12">
-            <h2 className="text-3xl font-extrabold text-gray-900">Pricing</h2>
-            <p className="mt-4 text-lg text-gray-500">
-              Our pricing is transparent so that you know what you are being charged for.
-            </p>
+  return (
+    <section id="pricing" className="bg-[#F7F8FC] py-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl font-extrabold text-gray-900 font-volkhov">Pricing</h2>
+          <p className="mt-4 text-lg text-[#595959] font-poppins">
+            Our pricing is transparent so that you know what you are being charged for.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-1 gap-4">
+          <div className="bg-white md:rounded-l-xl p-6 md:h-3/4 md:my-16">
+            <h3 className="text-xl font-bold text-gray-900 mb-4 font-poppins">Lite</h3>
+            <p className="text-gray-600 mb-4 font-poppins">Landing Page</p>
+            <div className="flex items-baseline mb-6">
+              <span className="text-5xl font-extrabold text-gray-900 font-poppins">$1000</span>
+              <span className="text-[#595959] ml-1 font-poppins">/wk</span>
+            </div>
+            <ul className="list-disc list-inside text-gray-600 mb-6 font-poppins">
+              <li>UI/UX design</li>
+              <li>Frontend development</li>
+              <li>UX copy</li>
+            </ul>
+            <button className="w-full bg-white font-poppins text-orange-500 border border-orange-500 px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
+              Select
+            </button>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-3 md:gap-1 gap-4">
-            <div className="bg-white md:rounded-l-xl p-6 md:h-3/4 md:my-16">
-              <h3 className="text-xl font-bold text-gray-900 mb-4">Lite</h3>
-              <p className="text-gray-600 mb-4">Landing Page</p>
-              <div className="flex items-baseline mb-6">
-                <span className="text-5xl font-extrabold text-gray-900">$1000</span>
-                <span className="text-gray-500 ml-1">/wk</span>
-              </div>
-              <ul className="list-disc list-inside text-gray-600 mb-6">
-                <li>UI/UX design</li>
-                <li>Frontend development</li>
-                <li>UX copy</li>
-              </ul>
-              <button className="w-full bg-white text-orange-500 border border-orange-500 px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
-                Select
-              </button>
+          {/* Add the other two pricing cards here */}
+          <div className="bg-white p-6 md:h-5/6 md:my-12">
+            <h3 className="text-xl font-bold text-gray-900 mb-4 font-poppins">Basic</h3>
+            <p className="text-gray-600 mb-4 font-poppins">Websites and webapp</p>
+            <div className="flex items-baseline mb-6">
+              <span className="text-5xl font-extrabold text-gray-900 font-poppins">$1500</span>
+              <span className="text-[#595959] ml-1 font-poppins">/wk</span>
             </div>
-            {/* Add the other two pricing cards here */}
-            <div className="bg-white p-6 md:h-5/6 md:my-12">
-              <h3 className="text-xl font-bold text-gray-900 mb-4">Basic</h3>
-              <p className="text-gray-600 mb-4">Websites and webapp</p>
-              <div className="flex items-baseline mb-6">
-                <span className="text-5xl font-extrabold text-gray-900">$1500</span>
-                <span className="text-gray-500 ml-1">/wk</span>
-              </div>
-              <ul className="list-disc list-inside text-gray-600 mb-6">
-                <li>UI/UX design</li>
-                <li>Frontend development</li>
-                <li>UX copy</li>
-                <li>Maintenance</li>
-              </ul>
-              <button className="w-full bg-white text-orange-500 border border-orange-500 px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
-                Select
-              </button>
-            </div>
-            <div className="bg-black py-16 px-8 text-white rounded-xl">
+            <ul className="list-disc list-inside text-gray-600 mb-6 font-poppins">
+              <li>UI/UX design</li>
+              <li>Frontend development</li>
+              <li>UX copy</li>
+              <li>Maintenance</li>
+            </ul>
+            <button className="w-full bg-white font-poppins text-orange-500 border border-orange-500 px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
+              Select
+            </button>
+          </div>
+          <div className="bg-black py-16 px-8 text-white rounded-xl">
             <div className="max-w-4xl mx-auto">
-                <h1 className="text-4xl font-bold mb-4 text-[#3FDD78]">Pro</h1>
-                <h2 className="text-2xl mb-8">AI Feature</h2>
-                <p className="text-6xl font-bold mb-8">$3500<span className="text-2xl">/wk</span></p>
-                <ul className="list-disc pl-6 mb-8">
+              <h1 className="text-4xl font-bold mb-4 text-[#3FDD78] font-poppins">Pro</h1>
+              <h2 className="text-2xl mb-8">AI Feature</h2>
+              <p className="text-6xl font-bold mb-8 font-poppins">$3500<span className="text-2xl font-poppins">/wk</span></p>
+              <ul className="list-disc pl-6 mb-8 font-poppins">
                 <li>Data preparation and processing</li>
                 <li>Model building and integration</li>
                 <li>Model fine tuning</li>
                 <li>Model deployment and maintenance</li>
-                <li>Data collection <span className="bg-[#FF97E8] text-[#222222] px-2 py-1 rounded-md ml-2">+$300/1,000 data</span></li>
-                </ul>
-                <button className="w-full text-white bg-[#FA7436] px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
+                <li>Data collection <span className="bg-[#FF97E8] text-[#222222] px-2 py-1 rounded-md ml-2 font-poppins">+$300/1,000</span></li>
+              </ul>
+              <button className="w-full text-white font-poppins bg-[#FA7436] px-6 py-3 rounded-md shadow-sm hover:bg-orange-500 hover:text-white transition-colors duration-300">
                 Select
               </button>
             </div>
-            </div>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Pricing;
+      </div>
+    </section>
+  );
+};
+
+export default Pricing;
